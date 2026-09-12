b3f3ec5f41ba0dad5d667d8293dad5a5f1b57626
DominikKoniarz <dominik.koniarz.development@gmail.com>
2026-09-12 22:58:25 +0200

    Add shadcn (button and dialog)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
         <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="bg-pink-500 h-full w-full flex-1"></main>
+            <main className="bg-pink-500 h-full w-full flex-1">
+                <Button type="button" onClick={() => alert("Hello")}>
+                    Click me
+                </Button>
+            </main>
         </div>
     );
 }
